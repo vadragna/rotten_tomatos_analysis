e8d047bc1377171a0b22d981f8671f7fe1186644
Valerio Adragna <valerioadragna89@hotmail.it>
2023-09-27 18:19:54 +0200

    added link to the trailer

--- Streamlit/advanced_recommender.py
+++ Streamlit/advanced_recommender.py
@@ -61,6 +61,13 @@ def advanced_recommender():
         else:
             return None
 
+    
+
+    def generate_youtube_search_url(movie_title):
+        query = '+'.join(movie_title.split())
+        youtube_url = f"https://www.youtube.com/results?search_query={query}+trailer"
+        return youtube_url
+
     def from_id_get_other_films_reviews_by_other_authors(x):
         movie_id_count = {}  
         subset = grouped_positive_reviews[grouped_positive_reviews['id'] == x]
@@ -80,9 +87,9 @@ def advanced_recommender():
         
         sorted_items = sorted(movie_id_count.items(), key=lambda x: x[1], reverse=True)
 
-        top_ten_items = sorted_items[:20]
+        top_fourty_items = sorted_items[:40]
                 
-        return top_ten_items
+        return top_fourty_items
 
     def get_recommendation_in_cluster(x):
         subset = films[films['clusters'] == x].head(10)
@@ -92,8 +99,37 @@ def advanced_recommender():
             random_recommendations = random.sample(top_10_recommendations['title'].tolist(), 3)
             for recommendation in random_recommendations:
                 st.write(recommendation)
+                return recommendation
         except:
             st.write('could not find any recommendation for the inserted title')
+    
+    def get_link_and_extras_from_result(x):
+        time.sleep(5)
+        base_url = 'http://www.omdbapi.com/'
+        params_extra = {
+        't': x,  
+        'apikey': api_key,               
+        }
+        
+        print(params_extra)
+
+        for retry in range(5):
+            try:
+                response_extra = requests.get(base_url, params=params_extra)
+
+                if response_extra.status_code == 200:
+                    movie_data = response_extra.json()  
+                    return movie_data['Year'], movie_data['Director'], movie_data['Plot'], movie_data['Language'], movie_data['Poster']
+            except requests.ConnectionError:
+                print('API request for extra info failed, will retry')
+                time.sleep(10)
+
+    def show_more_info(x):
+        st.write('Year: ', x[0])
+        st.write('Director: ', x[1])
+        st.write('Plot: ', x[2])
+        st.write('Language(s): ', x[3])
+        st.image(x[4], caption='Movie Poster')
 
     def better_recommender():
     
@@ -104,8 +140,14 @@ def advanced_recommender():
             other_reviews_recommender = from_id_get_other_films_reviews_by_other_authors(movie_id)
 
             if not other_reviews_recommender:
-                st.write('No similar movies found based on other reviews.')
-                return get_recommendation_in_cluster(cluster)
+                st.write('No similar movies found based on other reviews. You might like this from the same cluster: ')
+                title = get_recommendation_in_cluster(cluster)
+                try:
+                    more_info = get_link_and_extras_from_result(title)
+                    show_more_info(more_info)
+                    st.write(generate_youtube_search_url(title))
+                except:
+                    st.write('Not extra info found')
             
             for other_reviews in other_reviews_recommender:
                 movie_id = other_reviews[0]
@@ -114,10 +156,27 @@ def advanced_recommender():
                 if not matching_movies.empty and matching_movies.values[0] == cluster:
                     st.write('Found a movie according to other reviewers:', title)
                     st.write('Found a movie according to other reviewers that matches also clusters:', title)
+                    more_info = get_link_and_extras_from_result(title)
+                    try:
+                        show_more_info(more_info)
+                        st.write(generate_youtube_search_url(title))
+                    except:
+                        st.write('No more info found: ')
                     return other_reviews
                 else:
-                    st.write('Recommendation based on other reviewers', other_reviews_recommender[0][0], ' ', other_reviews_recommender[1][0], ' ', other_reviews_recommender[2][0])
+                    rec_title = get_title_from_id(other_reviews_recommender[0][0])
+                    st.write('Recommendation based on other reviewers :', rec_title)
+                    more_info = get_link_and_extras_from_result(rec_title)
+                    show_more_info(more_info)
+                    st.write(generate_youtube_search_url(rec_title))
                     st.write('You might also like these other movies from the same cluster: ')
-                    return get_recommendation_in_cluster(cluster)
+                    title = get_recommendation_in_cluster(cluster)
+                    try:
+                        more_info = get_link_and_extras_from_result(title)
+                        show_more_info(more_info)
+                        st.write(generate_youtube_search_url(title))
+                    except:
+                        st.write('No more info found: ')
+                    return 
     
     better_recommender()
